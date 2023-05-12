ab83a7570565d228448cda04f405fa1297a39287
marcelinoMSR <marcelino-sanchez2011@hotmail.com>
2023-05-12 17:38:57 -0600

    avance1

--- proyectoFinalRA.R
+++ proyectoFinalRA.R
@@ -1,3 +1,8 @@
 ###
 ### 
-datosCrudos=
+datosCrudos=read.csv("crimenes2022.csv",header=TRUE)
+
+datosCrudos$OFFENSE_DESCRIPTION=as.factor(datosCrudos$OFFENSE_DESCRIPTION)
+
+datosCrudos <- datosCrudos %>%
+                
